2834cb0b4d7fbdf947aaa2cf104629257f47f720
greg <gtemp@boximail.com>
2022-10-05 22:06:34 +0800

    Start chapter 4

---
+++ src/main/scala/creativescala4.scala
@@ -0,0 +1,9 @@
+import doodle.core._
+import doodle.image._
+import doodle.image.syntax.all._
+import doodle.image.syntax.core._
+import doodle.java2d._
+
+object Example4 {
+    val image = Image.circle(100).fillColor(Color.paleGoldenrod).strokeColor(Color.indianRed)
+}
